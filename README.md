1c192a8c82d6e40db9bf4b23dd9537f637d62059
“Teo” <matiussw@gmail.com>
2026-02-24 12:41:45 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,15 @@
+Comando para subir a GitHub
+
+
+git init 
+
+git remote add origin https://github.com/matiussw/NodeJsApiExpress.git
+
+git add .
+
+git commit -m "Message"
+
+git push (A la primera sale error )
+
+
+git push --set-upstream origin main
--- index.js
+++ index.js
@@ -3,11 +3,31 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-
 app.get('/hola/aprende/apis', (req, res) => {
   res.send('Hello World!');
 });
 
+// Datos de ejemplo
+const usuarios = [
+  { id: 1, nombre: 'Mateo', rol: 'Cloud' },
+  { id: 2, nombre: 'Ana', rol: 'Developer' },
+];
 
-app.listen(port, () => {console.log("Server esta arriba" + port)});
+// GET - Obtener todos los usuarios
+app.get('/usuarios', (req, res) => {
+  res.json({ success: true, data: usuarios });
+});
+
+// GET - Obtener un usuario por ID
+app.get('/usuarios/:id', (req, res) => {
+  const usuario = usuarios.find(u => u.id === parseInt(req.params.id));
+  if (!usuario) {
+    return res.status(404).json({ success: false, message: 'Usuario no encontrado' });
+  } else {
+    res.json({ success: true, data: usuario });
+  }
+
+});
+
+app.listen(port, () => { console.log("Server esta arriba" + port) });
 
