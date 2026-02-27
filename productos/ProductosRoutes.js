d5b1248b916f78c024955a4762f9fd404f49eee7
“Teo” <matiussw@gmail.com>
2026-02-27 13:33:44 -0500

    Nodemon And ApiSecurity

--- productos/ProductosRoutes.js
+++ productos/ProductosRoutes.js
@@ -2,27 +2,128 @@ const express = require('express');
 const router = express.Router();
 
 const productos = [
-{id:1, nombre:'Pc',precio: 2000, descripcion: 'Computadora de escritorio', stock: 10, categoria: 'Electrónica'},
-{id:2, nombre:'Laptop',precio: 3000, descripcion: 'Computadora portátil', stock: 5, categoria: 'Electrónica'},
-{id:3, nombre:'Mouse',precio: 50, descripcion: 'Mouse inalámbrico', stock: 20, categoria: 'Electrónica'}
+    { id: 1, nombre: 'Pc', precio: 2000, descripcion: 'Computadora de escritorio', stock: 10, categoria: 'Computacion' },
+    { id: 2, nombre: 'Laptop', precio: 3000, descripcion: 'Computadora portátil', stock: 5, categoria: 'Electrónica' },
+    { id: 3, nombre: 'Mouse', precio: 50, descripcion: 'Mouse inalámbrico', stock: 20, categoria: 'Electrónica' }
+];
 
-];    
 
-// GET - Obtener todos los productos
 router.get('/productos', (req, res) => {
-    res.json({ success: true, data: productos });
+
+    const apiKey = req.headers['password'];
+    if (!apiKey) {
+        res.status(401).json(
+            {
+                success: false, message:
+                    'API key es requerida'
+            });
+    }
+    if (apiKey !== 'Hola Mundo') {
+        res.status(403).json(
+            {
+                success: false, message:
+                    'Error la password no es correcta'
+            });
+    }
+
+    const { nombre, precio, descripcion, stock, categoria } = req.query;
+    let filteredProducts = productos.filter(p => {
+        return ((!nombre || p.nombre.toLowerCase().includes(nombre.toLowerCase())) &&
+            (!precio || p.precio === parseFloat(precio)) &&
+            (!descripcion || p.descripcion.toLowerCase().includes(descripcion.toLowerCase())) &&
+            (!stock || p.stock === parseInt(stock)) &&
+            (!categoria || p.categoria.toLowerCase().includes(categoria.toLowerCase()))
+        )
+    }
+    );
+    res.json({ success: true, data: filteredProducts });
 });
 
 // GET - Obtener un producto por ID
 router.get('/productos/:id', (req, res) => {
-    const product  = productos.find(u => u.id === parseInt(req.params.id));
+
+    const apiKey = req.headers['password'];
+    if (!apiKey) {
+        res.status(401).json(
+            {
+                success: false, message:
+                    'API key es requerida'
+            });
+    }
+    if (apiKey !== 'Hola Mundo') {
+        res.status(403).json(
+            {
+                success: false, message:
+                    'Error la password no es correcta'
+            });
+    }
+    const product = productos.find(u => u.id === parseInt(req.params.id));
     if (!product) {
         return res.status(404).json({ success: false, message: 'Producto  no encontrado' });
     } else {
         res.json({ success: true, data: product });
     }
+});
+
+router.post('/productos', (req, res) => {
+    const { nombre, precio, descripcion, stock, categoria } = req.body;
+    if (!nombre || !precio || !descripcion || !stock || !categoria) {
+        return res.status(400).json({ success: false, message: 'Faltan datos requeridos' });
+    }
+
+    const apiKey = req.headers['password'];
+    if (!apiKey) {
+        res.status(401).json(
+            {
+                success: false, message:
+                    'API key es requerida'
+            });
+    }
+    if (apiKey !== 'Hola Mundo') {
+        res.status(403).json(
+            {
+                success: false, message:
+                    'Error la password no es correcta'
+            });
+    }
+    const newProduct = {
+        id: productos.length + 1,
+        nombre,
+        precio,
+        descripcion,
+        stock,
+        categoria
+    };
+    productos.push(newProduct);
+    res.status(201).json({ success: true, data: newProduct });
 
 });
 
+router.delete('/productos/:id', (req, res) => {
+    const apiKey = req.headers['password'];
+    if (!apiKey) {
+        res.status(401).json(
+            {
+                success: false, message:
+                    'API key es requerida'
+            });
+    }
+    if (apiKey !== 'Hola Mundo') {
+        res.status(403).json(
+            {
+                success: false, message:
+                    'Error la password no es correcta'
+            });
+    }
 
-module.exports = router;
+    const productIndex = productos.findIndex(u => u.id === parseInt(req.params.id));
+    if (productIndex === -1) {
+        return res.status(404).json({ success: false, message: 'Producto no encontrado' });
+    }
+    productos.splice(productIndex, 1);
+    
+    res.status(201).json({ success: true, data: "El Producto se ha eliminado" });
+});
+
+
+module.exports = router;    
--- usuarios/UsuariosRoutes.js
+++ usuarios/UsuariosRoutes.js
@@ -9,11 +9,43 @@ const usuarios = [
 
 // GET - Obtener todos los usuarios
 router.get('/usuarios', (req, res) => {
+     const apiKey = req.headers['password'];
+    if (!apiKey) {
+        res.status(401).json(
+            {
+                success: false, message:
+                    'API key es requerida'
+            });
+    }
+    if (apiKey !== 'Hola Mundo') {
+        res.status(403).json(
+            {
+                success: false, message:
+                    'Error la password no es correcta'
+            });
+    }
+
     res.json({ success: true, data: usuarios });
 });
 
 // GET - Obtener un usuario por ID
 router.get('/usuarios/:id', (req, res) => {
+     const apiKey = req.headers['password'];
+    if (!apiKey) {
+        res.status(401).json(
+            {
+                success: false, message:
+                    'API key es requerida'
+            });
+    }
+    if (apiKey !== 'Hola Mundo') {
+        res.status(403).json(
+            {
+                success: false, message:
+                    'Error la password no es correcta'
+            });
+    }
+
     const usuario = usuarios.find(u => u.id === parseInt(req.params.id));
     if (!usuario) {
         return res.status(404).json({ success: false, message: 'Usuario no encontrado' });
